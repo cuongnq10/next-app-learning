294e27b559271097c46a4ce5a348c7ee8ec9fad5
Cuong NQ1 <cuongnq1@bsscommerce.com>
2026-01-11 22:18:50 +0700

    new commit

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,10 @@
 'use server';
-import { signIn, signOut } from './auth';
+import { auth, signIn, signOut } from './auth';
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+}
 
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' });
