11b98d8f7e717ee92b9b2cdb067b913c986ad500
SBC - CuongNQ1 <cuongnq1@bsscommerce.com>
2026-01-13 17:38:03 +0700

    new commit

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,6 +2,7 @@
 import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import { supabase } from './supabase';
+import { getBookings } from './data-service';
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -18,7 +19,21 @@ export async function updateGuest(formData) {
     .eq('id', session.user.guestId);
 
   if (error) throw new Error('Guest could not be updated');
-  revalidatePath('/account/profile')
+  revalidatePath('/account/profile');
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+  const guestBookings = await getBookings(session.user.guestId); 
+  const guestBookingIds = guestBookings.map(booking => booking.id); 
+  if (!guestBookingIds.includes(bookingId)) throw new Error('You are not allowed to delete this booking')
+  const { error } = await supabase.from('bookings').delete().eq('id', bookingId);
+
+  if (error) {
+    throw new Error('Booking could not be deleted');
+  }
+  revalidatePath('/account/reservations'); 
 }
 
 export async function signInAction() {
